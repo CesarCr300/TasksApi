cef484298b943b3ffa65cf9c8ace7e51d7915771
Cesar <cesarjcontreras30@gmail.com>
2024-11-28 13:34:44 -0500

    build: implementation of dynamic import of router

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,20 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+const routersLocation = ["./modules/tasks/routes/task.routes"];
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   try {
     const dataSource = DataSourceSingletone.get();
     await dataSource.initialize();
+    routersLocation.forEach(async (routerLocation) => {
+      const { router } = await import(routerLocation);
+      app.use("/", router);
+    });
     console.log(`Server running on port ${PORT}`);
   } catch (error) {
+    console.log(error);
     console.log("Server is not running");
   }
 });
