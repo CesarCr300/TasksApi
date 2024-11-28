c69e345935cbe874bc2713797508e7cef942b7b2
Cesar <cesarjcontreras30@gmail.com>
2024-11-28 12:35:09 -0500

    fix(database): correct configuration of database

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import { DataSourceSingletone } from "./database/datasource";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, async () => {
+  try {
+    const dataSource = DataSourceSingletone.get();
+    await dataSource.initialize();
+    console.log(`Server running on port ${PORT}`);
+  } catch (error) {
+    console.log("Server is not running");
+  }
 });
--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,13 +1,25 @@
 import { DataSource } from "typeorm";
 
-export const dataSource = new DataSource({
-  type: "mysql",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT as any),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: ["src/modules/*/entities/*.js"],
-  logging: true,
-  synchronize: true,
-});
+export class DataSourceSingletone {
+  private static dataSource: DataSource | null;
+  static get(): DataSource {
+    if (DataSourceSingletone.dataSource == null) {
+      DataSourceSingletone.initialize();
+    }
+    return DataSourceSingletone.dataSource;
+  }
+
+  static initialize() {
+    DataSourceSingletone.dataSource = new DataSource({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as any),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + "/../**/*.entity.{js,ts}"],
+      logging: true,
+      synchronize: false,
+    });
+  }
+}
